9c6514256eb17ffbf20ae3727bb6ad2ff33fc894
LouHV <lviet13zx@gmail.com>
2023-06-24 17:11:01 +0700

    first commit

---
+++ CRUD/Controllers/StudentController.cs
@@ -0,0 +1,93 @@
+﻿using CRUD.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        /*public ActionResult Index(int? page, int? pageSize)
+        {
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 2;
+            }
+            StudentList stuList = new StudentList();
+            List<Student> obj = stuList.GetStudent(String.Empty).OrderBy(x=>x.FullName).ToList();
+            return View(obj.ToPagedList((int)page,(int)pageSize));
+        }*/
+
+        public ActionResult Index(string strSearch, int? page, int? pageSize)
+        {
+            if (page == null)
+            {
+                page = 1;
+            }
+            if (pageSize == null)
+            {
+                pageSize = 2;
+            }
+
+            StudentList stuList = new StudentList();
+            List<Student> obj = stuList.GetStudent(String.Empty).OrderBy(x => x.FullName).ToList();
+            if (!string.IsNullOrEmpty(strSearch))
+            {
+                obj = obj.Where(x => x.FullName.Contains(strSearch)).ToList();
+            }
+            return View(obj.ToPagedList((int)page, (int)pageSize));
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student stu)
+        {
+            if (ModelState.IsValid)
+            {
+                StudentList stuList = new StudentList();
+                stuList.AddStudent(stu);
+                return RedirectToAction("Index");
+                
+            }
+            return View();
+        }
+        public ActionResult Edit(String id = " ")
+        {
+            StudentList stuList = new StudentList();
+            List<Student> obj = stuList.GetStudent(id);
+            return View(obj.FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Edit(Student stu)
+        {
+            StudentList stuList = new StudentList();
+            stuList.UpdateStudent(stu);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(string id = " ")
+        {
+            StudentList stuList = new StudentList();
+            List<Student> obj = stuList.GetStudent(id);
+            return View(obj.FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Delete(Student stu)
+        {
+            StudentList stuList = new StudentList();
+            stuList.DeleteStudent(stu);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUD/Models/Student.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage ="Mời nhập Họ và tên ")]
+        [Display(Name = "Họ và tên: ")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Mời Address ")]
+        [Display(Name = "Địa chỉ: ")]
+        public string Address { get; set; }
+
+    }
+    class StudentList
+    {
+        DBConnection db;
+        public StudentList()
+        {
+            db = new DBConnection();
+        }
+        public List<Student> GetStudent(string ID)
+        {
+            string sql;
+            if(string.IsNullOrEmpty(ID))
+            {
+                sql = "select * from Students ";
+            }
+            else
+            {
+                sql = "select * from Students where ID = " + ID;
+            }
+            List<Student> stuList =new List<Student>();
+            DataTable dt = new DataTable();
+            SqlConnection con = db.getConnection();
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            con.Open();
+            da.Fill(dt);
+            da.Dispose();
+            con.Close();
+            Student tmpstu;
+            for (int i = 0; i <dt.Rows.Count; i++){
+                tmpstu = new Student();
+                tmpstu.ID = Convert.ToInt32(dt.Rows[i]["ID"].ToString());
+                tmpstu.FullName = dt.Rows[i]["FullName"].ToString();
+                tmpstu.Address = dt.Rows[i]["Address"].ToString();
+                stuList.Add(tmpstu);
+            }
+            return stuList;
+        }
+        public void AddStudent(Student stu)
+        {
+            string sql = " INSERT INTO Students(FullName,Address) VALUES (N'" + stu.FullName +
+                "', N'" + stu.Address + "')";
+            SqlConnection con = db.getConnection();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
+        public void UpdateStudent(Student stu)
+        {
+            string sql = " update Students set FullName = N'" + stu.FullName + "',Address = N'" + stu.Address + "' where  ID = "+stu.ID;
+            SqlConnection con = db.getConnection();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
+        
+        public void DeleteStudent(Student stu)
+        {
+            string sql = " delete Students where  ID = " + stu.ID;
+            SqlConnection con = db.getConnection();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
+    }
+}
